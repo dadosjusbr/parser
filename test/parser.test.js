68c027eb93785387d1f29386a1ae43b8f8f41cf5
Vinicius <viniciusaagostini@gmail.com>
2019-02-27 00:24:53 -0300

    implement and test the function responsable for finding the sheet header line

---
+++ test/parser.test.js
@@ -0,0 +1,52 @@
+const parser = require('../src/parser');
+
+describe('paser parse', () => {
+  it('shoul throw not implemented error', () => {
+    try {
+      parser.parse();
+      fail('an error should have been thrown');
+    } catch(e) {
+      expect(e.message).toEqual('not implemented yet');
+    }
+  });
+});
+
+describe('parser _getHeaderLine ', () => {
+  it('should return undefined if the sheet is empty', () => {
+    const emptySheet1 = [],
+          emptySheet2 = [[]],
+          emptySheet3 = [[], [], []],
+          emptySheet4 = [['','',''], []];
+    
+    [emptySheet1, emptySheet2, emptySheet3, emptySheet4].forEach(sheet => {
+      expect(parser._getHeaderLine(sheet)).toBe(undefined);
+    });
+  });
+
+  it('should return undefined if the sheet not contains the header', () => {
+    const sheetMock = [
+      ['anything', 'anything2'],
+      ['otherstuff', 'anything2'],
+      ['cpf', 'notName'],
+      ['not the word above', 'nome']
+    ];
+    expect(parser._getHeaderLine(sheetMock)).toBe(undefined);
+  });
+
+  it('should return the line containing the sheet header', () => {
+    const sheetMock1 = [['cpf', 'nome']],
+          sheetMock2 = [['anything', 'anything2'], ['cpf', 'nome']],
+          sheetMock3 = [['anything', 'anything2'], ['anything', 'anything2'], ['cpf', 'nome']];
+    
+    [sheetMock1, sheetMock2, sheetMock3].forEach((sheet, index) => {
+      expect(parser._getHeaderLine(sheet)).toBe(index+1);    
+    });
+  });
+
+  it('should return the line containing the sheet header even when the cell value is dirty', () => {
+    const sheetMock = [
+      ['CPF (só números)', 'Nome with anything']
+    ];
+    expect(parser._getHeaderLine(sheetMock)).toBe(sheetMock.length);    
+  });
+});
