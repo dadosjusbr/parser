114fe3b8881dfe4e7199e2491af6393e16c6d4df
Vinicius <viniciusaagostini@gmail.com>
2019-04-18 09:53:17 -0300

    experimental stuff

--- src/parser_service.js
+++ src/parser_service.js
@@ -51,4 +51,47 @@ const handleError = (err, res) => {
   });
 };
 
+const { promisify } = require('util');
+const fs = require('fs');
+const parser = require('./parser');
+
+const spreadSheetIterator = async () => {
+  const spreasheetsPath = __dirname + '/spreadsheets';
+  const filesPaths = await promisify(fs.readdir)(spreasheetsPath);
+  console.log(filesPaths);
+  const promises = filesPaths.map(filePath => promisify(fs.readFile)(`${spreasheetsPath}/${filePath}`));
+  const files = await Promise.all(promises);
+  console.log(files);
+  const set = new Set();
+  const fileNamesCols = {};
+  files.forEach((file, index) => {
+    try {
+      //if(index > 30) return;
+      //if(filesPaths[index] !== '79572b97c96c5979d3fd9e00e101d4e2.xls') return;
+      //if(filesPaths[index] !== 'fe23d47849cc45a194634d0b07589b46.xls') return;
+      console.log('===================== ' + filesPaths[index]);
+      const spreadsheet = xlsxService.convertSpreadsheetToJson(file);
+      const subsidio = parser._getSubsidioData(spreadsheet);
+      const sheet = parser._getSheet('pessoais', spreadsheet);
+      const header = parser._getHeader(sheet);
+      set.add(JSON.stringify(header));
+      fileNamesCols[JSON.stringify(header)] = 
+        fileNamesCols[JSON.stringify(header)] ? [...fileNamesCols[JSON.stringify(header)], filesPaths[index]] : [filesPaths[index]];  
+      //console.log(contracheque);
+      //console.log(filesPaths[index] + ' -> ok');
+      console.log(subsidio);
+      //console.log(filesPaths[index]);
+    } catch (e) {
+      console.log(filesPaths[index] + ' -> error');
+      console.log(e);
+    }
+  });
+  //console.log(JSON.stringify(fileNamesCols));
+  //console.log([...set]);
+  //console.log([...set].length);
+  //console.log(spreadSheets);
+};
+
+spreadSheetIterator();
+
 module.exports = { getParsedSpreadsheet, _fetchSpreadshet };
---
+++ src/result.js
@@ -0,0 +1,62 @@
+const results = [ 
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total Vantagens Pessoais",null,"OBSERVAÇÃO: Se houver alguma VANTAGEM PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de Permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total Vantagens Pessoais",null,"OBSERVAÇÃO: Se houver alguma VANTAGEM PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra 1 DIR.PES. (R$)","Detalhe","Outra 2 DIR.PES. (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais"]',
+  '[null,null,"Abono de permanência (R$)","Vantagens Art. 184, I, e 192, I, Lei 1.711/52 (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais","OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver alguma VANTAGEM PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total Direitos Pessoais",null,"OBSERVAÇÃO: Se houver alguma VANTAGEM PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência", "Outra - Detalhe","Outra - (R$)","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Vant. Art. 192, I\\nda Lei 8.112/90","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Abono de Permanência-GNAT (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$) ","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total Vantagens Pessoais",null,"OBSERVAÇÃO: Se houver alguma VANTAGEM PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\"","cpfsrv","nmesrv","SomaDeSomaDeSomaDevalrub"]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)  ","Detalhe   \\"Vant.Pessoal\\"","Outra (R$)","Detalhe","Total Vantagens Pessoais",null,"OBSERVAÇÃO: Se houver alguma VANTAGEM PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Abono de Permanência-GNAT (R$)","Outra (R$)","Detalhe","Total de Direitos Pessoais",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]',
+  '[null,null,"Abono de permanência (R$)","Outra (R$)","Detalhe","Outra (R$)","Detalhe"," ",null,"OBSERVAÇÃO: Se houver algum DIREITO PESSOAL que for diferente de Abono de Permanência, o valor deverá ser informado na coluna \\"OUTRA\\" e a discriminação da vantagem na coluna \\"Detalhe\\""]' ];
+
+  [ 
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total Vantagens Pessoais', null ],
+  [ null, null, 'Abono de Permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total Vantagens Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra 1 DIR.PES. (R$)', 'Detalhe', 'Outra 2 DIR.PES. (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null, null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais' ],
+  [ null, null, 'Abono de permanência (R$)', 'Vantagens Art. 184, I, e 192, I, Lei 1.711/52 (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)','Detalhe', 'Outra (R$)', 'Detalhe','Total de Direitos Pessoais' ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência', 'Outra - Detalhe', 'Outra - (R$)', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Vant. Art. 192, I\nda Lei 8.112/90', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Abono de Permanência-GNAT (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$) ', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total Vantagens Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null, 'cpfsrv', 'nmesrv', 'SomaDeSomaDeSomaDevalrub' ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)  ', 'Detalhe   "Vant.Pessoal"', 'Outra (R$)', 'Detalhe', 'Total Vantagens Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Abono de Permanência-GNAT (R$)', 'Outra (R$)', 'Detalhe', 'Total de Direitos Pessoais', null ],
+  [ null, null, 'Abono de permanência (R$)', 'Outra (R$)', 'Detalhe', 'Outra (R$)', 'Detalhe', ' ', null ] 
+  ]
+
+  String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+  };
+  
+  console.log(results.map(h => {
+    let header;
+    try {
+      header = JSON.parse(h);
+    } catch (e) {
+      console.log('o problema está nesse aqui: ');
+      console.log(h);
+    }
+    return header.filter(col => !col || !col.includes('OBSERVAÇÃO')).map(col => !col ? col : col.replace('(R$)', '').replaceAll(' ', '').split('-')[0]);
+  }));
