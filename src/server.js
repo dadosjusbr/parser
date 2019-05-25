d631de8fead0ea0e6fe7a69316278020623f7b63
Vinicius <viniciusaagostini@gmail.com>
2019-05-25 18:59:45 -0300

    add pino logger

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')({
+  prettyPrint: {
+    levelFirst: true,
+    colorize: true
+  },
+  prettifier: require('pino-pretty')
+});
 
 const parserService = require('./parser_service');
 const schemaService = require('./schema_service');
 
 const app = express();
-app.use(bodyParser.raw({ 
+app.use(pino)
+app.use(bodyParser.raw({
   type: () => true, //accept any content type
-  limit: '50mb' 
+  limit: '50mb'
 }));
 
 app.get('/', parserService.parseByUrl, parserService.getParsedSpreadsheet);
